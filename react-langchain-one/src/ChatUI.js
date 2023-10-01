b1190537946c3078f05dfa8a00a16d9137dd8f6a
Omprakash Sah Kanu <opsworld.g@gmail.com>
2023-10-01 13:12:49 -0600

    trying to integrate langchain

--- react-langchain-one/src/ChatUI.js
+++ react-langchain-one/src/ChatUI.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 import "./css/ChatUI.css";
 import { AiOutlineSend } from "react-icons/ai";
+import { OpenAI } from "langchain/llms/openai";
 
-function ChatUI({userName}) {
+async function chatModel(message) {
+  const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+  const llm = new OpenAI({
+    openAIApiKey: apiKey,
+    temperature: 0.9,
+    maxTokens: 150,
+    maxRetries: 2,
+    maxConcurrency: 2,
+  });
+  return await llm.predict(message);
+}
 
+function ChatUI({ userName }) {
   const [messages, setMessages] = useState([]);
+  
   const handleSendMessage = (event) => {
     event.preventDefault();
     const input = event.target.elements.message;
     const newMessage = input.value;
-    setMessages([...messages, newMessage]);
+    chatModel(newMessage).then((response) => {
+      console.log(response);
+      setMessages([...messages, response]);
+    });
+    // setMessages([...messages, newMessage]);
     input.value = "";
   };
 
@@ -31,7 +48,9 @@ function ChatUI({userName}) {
           name="message"
           placeholder="Type your message here"
         />
-        <button type="submit"><AiOutlineSend/></button>
+        <button type="submit">
+          <AiOutlineSend />
+        </button>
       </form>
     </div>
   );
